7a7dcab5101bb1bb82207dcb83e34b1695f477e4
Patrick Nellessen <patrickqwertz@gmail.com>
2024-12-20 18:08:26 +0100

    start gamemode cpp implementation

---
+++ Source/DAEGEF_UE5/TrainGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TrainGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DAEGEF_UE5_API ATrainGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	// Make BlueprintImplementableEvent functions for the stuff that needs BPs
+};
